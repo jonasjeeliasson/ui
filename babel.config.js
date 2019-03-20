81cfe7a4b1bef369dc4e7c22f1ef74a413d2654b
Jacob Blomgren <blomgren@protonmail.com>
2019-03-20 17:12:43 +0100

    docs: prop types wip

--- babel.config.js
+++ babel.config.js
@@ -18,7 +18,12 @@ module.exports = api => {
     '@babel/plugin-proposal-class-properties',
     'babel-plugin-styled-components',
     'ramda',
+    'babel-plugin-typescript-to-proptypes',
   ];
+
+  // if (process.env.NODE_ENV !== 'production') {
+  //   plugins.push('babel-plugin-typescript-to-proptypes');
+  // }
 
   return { comments: false, presets, plugins };
 };
