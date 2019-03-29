f23097442a68b1ebee2558bb7230e497408e1bbf
nordnet-release <nordnet-release@localhost>
2019-03-29 14:37:58 +0000

    Updates

---
+++ main.79925858dd7ba9acb1ed.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{400:function(n,o){},406:function(n,o,c){c(407),c(484),n.exports=c(904)},484:function(n,o,c){"use strict";c.r(o);c(485),c(660),c(721),c(724),c(879)},891:function(n,o){},897:function(n,o){}},[[406,1,2]]]);
--- main.87c45a25c445fb36e19e.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{399:function(n,o,p){p(400),p(477),n.exports=p(872)},477:function(n,o,p){"use strict";p.r(o);p(478),p(653),p(714),p(717)}},[[399,1,2]]]);
