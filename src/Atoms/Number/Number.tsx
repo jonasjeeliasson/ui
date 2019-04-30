923dfd819f638291643bb4fae4c63f3aa4fe0777
Jacob Blomgren <blomgren@protonmail.com>
2019-04-30 10:54:46 +0200

    feat(Number): add ticks

--- src/Atoms/Number/Number.tsx
+++ src/Atoms/Number/Number.tsx
@@ -1,22 +1,43 @@
 import React from 'react';
 import { injectIntl } from 'react-intl';
-import { NumberComponent as NumberComponentType } from './Number.types';
+import { NumberComponent as NumberComponentType, Ticks } from './Number.types';
 
 const getPrefix = (sign: boolean, value?: number | null) =>
   sign && !!value && value > 0 ? '+' : '';
+  
+const getTickDecimals = (value: number, ticks: Ticks) => {
+  if (!ticks || !value) {
+    return undefined;
+  }
 
-const getNumberOptions = (decimals?: number) =>
-  typeof decimals === 'undefined'
-    ? {}
-    : {
-        maximumFractionDigits: decimals,
-        minimumFractionDigits: decimals,
-      };
+  const tick = ticks.find(t => value >= t.fromPrice && value < t.toPrice + (t.tick || 0));
+
+  return tick ? tick.decimals : undefined;
+}
+
+const getNumberOptions = (value: number, ticks?: Ticks, decimals?: number) => {
+  if (typeof ticks !== 'undefined') {
+    const tick = getTickDecimals(value, ticks);
+    return {
+      maximumFractionDigits: tick,
+      minimumFractionDigits: tick,
+    };
+  }
+  if (typeof decimals !== 'undefined') {
+    return {
+      maximumFractionDigits: decimals,
+      minimumFractionDigits: decimals,
+    };
+  }
+
+  return {};
+}
 
 const NumberComponent: NumberComponentType = ({
   intl,
   value,
   decimals,
+  ticks,
   percentage = false,
   currency,
   sign = false,
@@ -27,7 +48,7 @@ const NumberComponent: NumberComponentType = ({
   return (
     <>
       {getPrefix(sign, value)}
-      {intl.formatNumber(value, getNumberOptions(decimals))}
+      {intl.formatNumber(value, getNumberOptions(value, ticks, decimals))}
       {percentage && '%'}
       {currency && ` ${currency}`}
     </>
